3711f02bc76c7cc332ae611f478f579239a860a7
SpiritasX <deja.bogovac@gmail.com>
2023-04-16 18:57:52 +0200

    Dodani admin, citalac i stavka repository

---
+++ src/main/java/com/example/demo/repository/CitalacRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Citalac;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CitalacRepository extends JpaRepository<Citalac, Long> {
+}
