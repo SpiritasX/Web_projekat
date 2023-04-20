29d79b4363deff07db09f837491d3fa678c40d01
SpiritasX <deja.bogovac@gmail.com>
2023-04-20 14:42:24 +0200

    Obrisana stavka
    - Stavka.java
    
    - StavkaRepository.java
    
    + Veza izmedju police i knjige
    
    + Veza izmedju recenzije i knjige

--- src/main/java/com/example/demo/repository/StavkaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Stavka;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StavkaRepository extends JpaRepository<Stavka, Long> {
-}
