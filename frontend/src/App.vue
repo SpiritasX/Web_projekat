114adb41fb41b325d6dd238694765c7218e08551
SpiritasX <deja.bogovac@gmail.com>
2023-06-23 17:53:23 +0200

    Neki css malo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,19 @@
 <template>
   <router-view/>
 </template>
+
+<style>
+html, body {
+  min-height: 100%;
+  display: grid;
+  position: relative;
+  margin: 0 0 0 0;
+}
+
+img {
+  height: 100%;
+  max-width: 100%;
+  display: block;
+  margin: auto;
+}
+</style>
