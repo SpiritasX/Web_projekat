6d224f7974fc7c536e18063426d3524dbbe2edfe
anastasijaterzic <anastasijaterzicc@gmail.com>
2023-06-23 19:53:46 +0200

    slike

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home-page">
     <h1 class="heading">BetterReads</h1>
+    <div class="bookshelf-image">
+      <img src="assets\knjige.jpg" alt=" knjige">
+    </div>
     <nav class="nav-links">
       <router-link class="nav-link" to="/prijavi-se">Prijavi se</router-link>
       <router-link class="nav-link" to="/registruj-se">Registruj se</router-link>
@@ -14,29 +17,40 @@
 <style>
 .home-page {
   text-align: center;
+  background-color: #cfe9f9;
+  padding: 20px;
 }
 
 .heading {
   font-size: 28px;
   margin-bottom: 20px;
   color: #333;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+}
+
+.bookshelf-image img {
+  width: 400px;
+  height: auto;
+  margin-bottom: 20px;
 }
 
 .nav-links {
   display: flex;
   justify-content: center;
+  list-style: none;
+  padding: 0;
 }
 
 .nav-link {
   margin: 0 10px;
   text-decoration: none;
-  color: #333;
+  color: #1e8cbf;
   font-weight: bold;
   transition: color 0.3s ease;
 }
 
 .nav-link:hover {
-  color: #ff6600;
+  color: #ff8080;
 }
 
 .nav-link:nth-child(1) {
@@ -44,19 +58,19 @@
 }
 
 .nav-link:nth-child(2) {
-  color: #9933cc;
+  color: #00b8a9;
 }
 
 .nav-link:nth-child(3) {
-  color: #ff3366;
+  color: #00a4d3;
 }
 
 .nav-link:nth-child(4) {
-  color: #66cc00;
+  color: #0089b3;
 }
 
 .nav-link:nth-child(5) {
-  color: #ff9900;
+  color: #00a7b5;
 }
 </style>
 
